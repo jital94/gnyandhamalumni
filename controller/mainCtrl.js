348e7ded91e68d4b7103372510d4e093159e34cd
SiddhantManocha <siddhantmanocha1994@gmail.com>
2016-05-14 18:45:26 +0530

    beta version

--- controller/mainCtrl.js
+++ controller/mainCtrl.js
@@ -2,41 +2,15 @@
 
 app.controller('MainCtrl', function ($scope,$rootScope) 
 {
-  $scope.order_item="Name";
-  $scope.order_reverse=false;
-
-  if (!$rootScope.studentData)
-  {
-    $rootScope.showLoad=true;
-  }
-  function showInfo(data, tabletop) 
-  {
-    $rootScope.showLoad=false;
-    for (var key in data) 
-    {
-      if (!data[key].Link_To_Profile_Image)
-        data[key].Link_To_Profile_Image="database/IDpics/"+data[key].Roll_No+".jpg"
-    }
-    $rootScope.studentData=data;
-    $scope.$apply();
-  }
-  $scope.initSpreadSheet = function()
-  {
-     Tabletop.init( { key: $rootScope.SETTINGS.spreadsheetLink,
-                     callback: showInfo,
-                     simpleSheet: true } );
-    
-
-  }
-
-  // http://plnkr.co/edit/Mlca2gXvXNVAXEsNbpCI?p=preview
-  $scope.customFilter = function (searchText) {
+  // http://plnkr.co/edit/Mlca2gXvXNVAXEsNbpCI?p=preview 
+  // custom filter on LOCAL_SETTINGS.searchInKeys
+  var customFilterFn = function (searchText) {
     function comparator(a, b) {
       return (''+a).toLowerCase().indexOf((''+b).toLowerCase()) > -1;
     }
     
-    var lookInKeys = ['Name', 'Position', 'Organization', 'Location', 'History_and_brief_bio'];
-    
+    var lookInKeys = $scope.LOCAL_SETTINGS.searchInKeys;
+
     return function (item) {
       if (!searchText) {
         return true; // no filter
@@ -53,16 +27,46 @@ app.controller('MainCtrl', function ($scope,$rootScope)
     };
   };
 
+  function loadStudentInfo(data, tabletop) 
+  {
+    $rootScope.showLoad=false; 
+    // by default don't show the load gif
+    // when load gif is displayed, we dont want to display the search box
+
+    // default profile picture for people
+    for (var key in data) 
+    {
+      if (!data[key].Link_To_Profile_Image)
+        data[key].Link_To_Profile_Image="database/IDpics/"+data[key].Roll_No+".jpg"
+    }
+    $rootScope.studentData=data;
+    $scope.$apply();
+  };
+
+  $scope.initSpreadSheet = function()
+  {
+     Tabletop.init( { key: $scope.LOCAL_SETTINGS.spreadsheetLink,
+                     callback: loadStudentInfo,
+                     simpleSheet: true } );
+    
+  };
+
   $scope.loadData = function ()
   {
     if (!$rootScope.studentData)
     {
+      $rootScope.showLoad=true;
+      // we load the settings twice once in mainCtrl and appCtrl to prevent race conditions
+      // in which the controller is loaded. This can be corrected later
       $.getJSON("settings.json", function(json) 
         {
-          $rootScope.SETTINGS=json;
+          $scope.LOCAL_SETTINGS=json;
+          $scope.customFilter=customFilterFn;
+          // filter is set now since filter requires the LOCAL_SETTINGS.searchInKeys 
+          // to be set
           $scope.$apply();
           $scope.initSpreadSheet();
         });
     } 
-}
+  };
 });
